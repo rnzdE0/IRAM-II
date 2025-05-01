a0c5d934ceb7e9edd6de5e2ff1900a7e0ab863b2
rnzdE0 <157136919+rnzdE0@users.noreply.github.com>
2025-05-01 22:57:26 +0800

    added Materials

--- IRAM-II/src/app/module/material/material.module.ts
+++ IRAM-II/src/app/module/material/material.module.ts
@@ -1,12 +1,45 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
 
-
+const MatModules = [
+  CommonModule,
+  MatButtonModule,
+  MatTableModule,
+  MatIconModule,
+  MatDividerModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatCardModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  ReactiveFormsModule,
+  FormsModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatNativeDateModule,  
+  MatInputModule,  
+  MatProgressSpinnerModule     
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [MatModules],
+  exports: [MatModules]
 })
-export class MaterialModule { }
+export class MaterialModule {}
