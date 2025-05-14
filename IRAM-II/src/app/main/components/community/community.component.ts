ac3d0e40766b147282e86963b15878d824713a9b
Jay-vee <131955010+JAY-VEE85@users.noreply.github.com>
2025-05-14 10:17:46 +0800

    Headers
    for some nav tab

--- IRAM-II/src/app/main/components/community/community.component.ts
+++ IRAM-II/src/app/main/components/community/community.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './community.component.scss'
 })
 export class CommunityComponent {
-
+  philippineTime: string = '';
+  
+  ngOnInit(): void {
+    this.updatePhilippineTime();
+    // Update the time every second
+    setInterval(() => this.updatePhilippineTime(), 1000);
+  }
+  
+  updatePhilippineTime(): void {
+    const now = new Date();
+    const options: Intl.DateTimeFormatOptions = {
+      timeZone: 'Asia/Manila',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true
+    };
+    
+    this.philippineTime = new Intl.DateTimeFormat('en-PH', options).format(now);
+  }
 }
