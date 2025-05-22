7bc591cb6a7ff81374e6ada8772a2e31c2c478e8
Don G <deaththekid9102@gmail.com>
2025-05-22 17:23:50 +0800

    added few updates
    added border rad sa root natin, nabago na din kulay ni footer copyright

--- IRAM-II/src/app/app.routes.ts
+++ IRAM-II/src/app/app.routes.ts
@@ -2,15 +2,9 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
-    //for admin//
-    {
-      path: 'admin',
-      loadChildren: () =>
-        import('./admin/admin.module').then(m => m.AdminModule)
-    },
 
-    { 
-        path: '', 
+    {
+        path: '',
         component: MainComponent,
         children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
