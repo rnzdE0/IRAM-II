b9fcb64e2b5a63e61c23f90e0ddb47eccce31d53
JoshP <joshearlconradpaclibar@gmail.com>
2025-05-25 21:23:03 +0800

    Footer Links Working

--- IRAM-II/src/app/main/components/about/components/academic-program/academic-program.component.ts
+++ IRAM-II/src/app/main/components/about/components/academic-program/academic-program.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MaterialModule } from '../../../../../module/material/material.module';
 
 @Component({
   selector: 'app-academic-program',
   imports: [MaterialModule],
   templateUrl: './academic-program.component.html',
-  styleUrl: './academic-program.component.scss'
+  styleUrls: ['./academic-program.component.scss']
 })
-export class AcademicProgramComponent {
+export class AcademicProgramComponent implements OnInit {
     selectedLevel = 'basicEd';
 
+    constructor(private route: ActivatedRoute) {}
+
+    ngOnInit() {
+        this.route.queryParams.subscribe(params => {
+            const level = params['level'];
+            if (level) {
+                this.selectedLevel = level;
+            }
+        });
+    }
+
     onLevelChange(newLevel: string): void {
-    this.selectedLevel = newLevel;
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
+        this.selectedLevel = newLevel;
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
 }
